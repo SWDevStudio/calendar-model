71c7d84d43c8dffcad937811d88d2504c747021a
Kotaro <swdevstudio@gmail.com>
2022-07-01 22:42:24 +0300

    created CalendarModule

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import CalendarYear from "./class/CalendarYear";
+import CalendarWeek from "./class/CalendarWeek";
+import CalendarDay from "./class/CalendarDay";
 import CalendarMonth from "./class/CalendarMonth";
 
-const calendarMonth = new CalendarMonth()
+export {
+  CalendarDay,
+  CalendarMonth,
+  CalendarWeek,
+  CalendarYear
+}
