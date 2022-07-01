eb0778f9403012428fb63cab882604ecac51faae
Kotaro <swdevstudio@gmail.com>
2022-07-01 19:56:37 +0300

    add method gridCalendarWithTime for CalendarWeek

--- src/class/CalendarWeek.ts
+++ src/class/CalendarWeek.ts
@@ -30,4 +30,24 @@ export default class CalendarWeek implements Calendar {
   swapPrevDate(): void {
     this.selectDate.add(-1, 'week')
   }
+
+  /**
+   * @description Возвращает массив моментов, с часами на каждый день.
+   * @param start {number} Начало отсчета для сетки в часах в 24 формате
+   * @param end  {number} Конец отсчета для сетки в часах в 24 формате
+   * @param step {number} Шаг цикла 0.5 - пол часа, 1 - час 2 - два часа и т.д
+   */
+  gridCalendarWithTime(start = 0, end = 24, step = 1): moment.Moment[] {
+    const arr = []
+    const startDay = this.selectDate.startOf('isoWeek')
+    for (let hour = start; hour < end / step; hour++) {
+      this.gridCalendar.forEach(i => {
+        arr.push(
+          moment(i.add(hour * step, 'hour'))
+        )
+      })
+    }
+
+    return arr
+  }
 }
