a107c4af75478bb0834e296105c06a30134b16ab
Kotaro <swdevstudio@gmail.com>
2022-07-03 23:16:47 +0300

    v0.2.0

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import CalendarWeek from "./class/CalendarWeek";
 import CalendarDay from "./class/CalendarDay";
 import CalendarMonth from "./class/CalendarMonth";
 
+const calendarWeek = new CalendarMonth({
+  startDay: 'sunday'
+})
+
+console.log(calendarWeek.gridCalendar())
+
 export {
   CalendarDay,
   CalendarMonth,
